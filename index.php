ccc3c090143af09c9f264eb29718e298de5e8a4b
Lukas Haug <Lukas Haug>
2017-03-11 14:15:09 +0100

    Testing magic

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <html>
-<head>
-<title>
-snek
-</title>
-<script src="snekScript.js" ></script>
-</head>
-<body>
-    <h1 id="scoreCounter" > Score: </h1>
-    <canvas id="daCanvas" width="800" height="800" style="position:aboslute;border:2px solid black; margin-left:auto; margin-right:auto;" >
-	Your browser does not support canvas!
-    </canvas>
-    
-</body>
+    <head>
+	<title>
+	    snek
+	</title>
+	<script src="snekScript.js" ></script>
+    </head>
+    <body>
+	<!-- Commenting for testing magit -->
+	<h1 id="scoreCounter" > Score: </h1>
+	<canvas id="daCanvas" width="800" height="800" style="position:aboslute;border:2px solid black; margin-left:auto; margin-right:auto;" >
+	    Your browser does not support canvas!
+	</canvas>
+	
+    </body>
 </html>
